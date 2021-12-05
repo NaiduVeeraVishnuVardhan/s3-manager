84e27732aca9db2ad8a5ba6e7bcd190f652e76ca
NaiduVeeraVishnuVardhan <60171846+NaiduVeeraVishnuVardhan@users.noreply.github.com>
2021-12-05 18:15:09 +1100

    added condition for creating the resource folder

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,18 @@ const router = express.Router();
 const appController = require("../controllers/appController");
 const multer = require("multer");
 const util = require("util");
+const fs = require("fs");
 const maxSize = 2 * 1024 * 1024;
 
 let storage = multer.diskStorage({
     destination: (req, file, cb) => {
+        if (!fs.existsSync('resources/static/assets/uploads/')) {
+            require('fs').mkdirSync(
+                'resources/static/assets/uploads/',
+                { recursive: true },
+                (err) => {}
+            )
+        }
         cb(null, "resources/static/assets/uploads/");
     },
     filename: (req, file, cb) => {
